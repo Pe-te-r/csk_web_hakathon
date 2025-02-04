ef3c0660778887e419c7eb8323764ba1df96d346
Pe-te-r <mburupeter8526@gmail.com>
2025-02-05 01:57:22 +0300

    front-end auth start

--- back-end/App/routes/__init__.py
+++ back-end/App/routes/__init__.py
@@ -1,6 +1,8 @@
 def register_bp(app):
     from App.routes.users import users_bp
     from App.routes.auth import auth_bp
+    from App.routes.products import products_bp
 
     app.register_blueprint(users_bp, url_prefix="/api")
+    app.register_blueprint(products_bp, url_prefix="/api")
     app.register_blueprint(auth_bp, url_prefix="/auth")

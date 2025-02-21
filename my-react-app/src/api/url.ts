6520295d742ea671eb3f765575613eb18b889ded
Pe-te-r <mburupeter8526@gmail.com>
2025-02-21 03:43:25 +0300

    more on url

--- my-react-app/src/api/url.ts
+++ my-react-app/src/api/url.ts
@@ -1,2 +1,2 @@
 // export const url = "http://localhost:5000/api"
-export const url = "https://cf02-102-0-18-196.ngrok-free.app/api"
+export const url ="https://7127-102-0-18-196.ngrok-free.app/api"

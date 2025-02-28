f55b760ca2b473a2fb4dc8614535abcb8f23362e
phantom <mburupeter8526@gmail.com>
2025-03-01 00:34:06 +0300

    more dynamic editing

--- back-end/App/routes/auth.py
+++ back-end/App/routes/auth.py
@@ -60,21 +60,16 @@ class LoginResource(Resource):
                     return f'missing field {field}',400
             
             user = User.get_by_email(data['email'])
-            print('one')
             if not user:
                 return 'user not found',404
-            print('one')
             if not user.active:
                 return 'user blocked',403
             
-            print('two')
             if not user.correct_password(data['password']):
                 return 'wrong password',401
-            print('five')
             token = jwt.create_access_token(user.email)
             
-            print('three')
-            return {'token':token,'id':str(user.id)},200
+            return {'token':token,'id':str(user.id),'role':user.role.value},200
 
         except Exception as e:
             print(e)

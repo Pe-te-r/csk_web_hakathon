032027625a588aa0e271629073520e215518b133
Pe-te-r <mburupeter8526@gmail.com>
2025-03-28 03:02:53 +0300

    more changes

--- back-end/app.py
+++ back-end/app.py
@@ -2,6 +2,6 @@ from App import create_app
 from config import Development
 
 
+app = create_app(Development)
 if __name__ == "__main__":
-    app = create_app(Development)
     app.run()
--- back-end/start.sh
+++ back-end/start.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-cd back-end  # Move into the backend directory
-gunicorn --bind 0.0.0.0:8000 app:app
+venv/bin/activate --bind 0.0.0.0:8000 app:app
